5c2b39dc2ecd6fc95dda95541976c2bf69c50a9e
Kala Sundararajan <ksundara@ads.uhnresearch.ca>
2016-04-21 09:57:36 -0400

    remove unnecessary scripts, add analysis

--- scripts/do.R
+++ scripts/do.R
@@ -14,11 +14,5 @@ source("Y:/LEAP/23. LEAP OA Data Quality/scripts/functions.R")
 #### load.R: load data ####
 source("scripts/load.R")
 
-#### clean.R: clean dataset, subset and derive variables ####
-source("scripts/clean.R")
-
-#### dict.R: create data dictionary for dataset ####
-source("scripts/dict.R")
-
-#### clean2.R: change KOOS variable names ####
-source("scripts/clean2.R")
+#### analysis.R: nested ANOVA analysis ####
+source("scripts/analysis.R")
